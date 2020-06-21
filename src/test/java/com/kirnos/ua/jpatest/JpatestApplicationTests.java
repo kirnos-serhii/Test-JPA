885397fa0987ae87d430f55f316f37c91d38e855
Kirnos Serhii <kirnosserhii@gmail.com>
2020-06-21 15:45:36 +0300

    Initial commit

---
+++ src/test/java/com/kirnos/ua/jpatest/JpatestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirnos.ua.jpatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpatestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
